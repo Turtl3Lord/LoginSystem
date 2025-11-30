c086093db3361145ae33be75bd308ea512da9ef2
Y4tendouji <pedrolucassouzacarneiro@outlook.com>
2025-11-30 16:40:45 -0300

    Fixed: CORS configuration

--- BackEnd/src/main/java/com/BackEnd/cors/CorsConfig.java
+++ BackEnd/src/main/java/com/BackEnd/cors/CorsConfig.java
@@ -17,7 +17,7 @@ public class CorsConfig {
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
                         .allowedOrigins(
-
+                                frontendUrl,
                                 "http://localhost:3000",
                                 "http://127.0.0.1:3000",
                                 "http://localhost:8080",
